eae74abf81a67aa2de5e449dfd894ebee5833993
Brozzor <46285168+Brozzor@users.noreply.github.com>
2021-10-05 16:19:46 +0200

    création du model ainsi que la migration

---
+++ database/migrations/2021_10_05_135058_peoples_migration.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PeoplesMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peoples', function(Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('height');
+            $table->integer('mass');
+            $table->string('hair_color');
+            $table->string('skin_hair');
+            $table->string('eye_color');
+            $table->string('birth_year');
+            $table->string('gender');
+            $table->string('homeworld');
+            $table->string('films');
+            $table->string('species');
+            $table->string('vehicles');
+            $table->string('starships');
+            $table->timestamp();
+            $table->string('url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peoples');
+    }
+}
