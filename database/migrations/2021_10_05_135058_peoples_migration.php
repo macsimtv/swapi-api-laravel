33f65a13546a255a8de6762df315390ef0d260b0
M1k3yD <85000131+M1k3yD@users.noreply.github.com>
2021-10-06 13:56:04 +0200

    [FIX] Correction du nom du champs 'skin_color' people migration

--- database/migrations/2021_10_05_135058_peoples_migration.php
+++ database/migrations/2021_10_05_135058_peoples_migration.php
@@ -6,36 +6,36 @@ use Illuminate\Support\Facades\Schema;
 
 class PeoplesMigration extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('peoples', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('name');
-            $table->integer('height');
-            $table->integer('mass');
-            $table->string('hair_color');
-            $table->string('skin_hair');
-            $table->string('eye_color');
-            $table->string('birth_year');
-            $table->string('gender');
-            $table->string('homeworld');
-            $table->string('url');
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('peoples', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+			$table->string('name');
+			$table->integer('height');
+			$table->integer('mass');
+			$table->string('hair_color');
+			$table->string('skin_color');
+			$table->string('eye_color');
+			$table->string('birth_year');
+			$table->string('gender');
+			$table->string('homeworld');
+			$table->string('url');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('peoples');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('peoples');
+	}
 }
