55b117d5c7145623ce1690b1d64a418dd63d96db
M1k3yD <85000131+M1k3yD@users.noreply.github.com>
2021-10-06 19:39:08 +0200

    Planet-Film Relation API

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -2,25 +2,12 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 use App\Models\Film;
-=======
->>>>>>> a29bc2ed615844b5b4ac3c4cc225e05e9600e708
 use Illuminate\Http\Request;
 
 class FilmController extends Controller
 {
-<<<<<<< HEAD
 	public function index($film_id)
 	{
-		$film = Film::find($film_id)->first();
-		$film['peoples'];
-		foreach ($film->peoples as $people) {
-			array_push($film['films'], $people->people);
-		}
-		return $film;
 	}
-=======
-    //
->>>>>>> a29bc2ed615844b5b4ac3c4cc225e05e9600e708
 }
--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -2,27 +2,19 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 use App\Models\Planet;
-=======
->>>>>>> a29bc2ed615844b5b4ac3c4cc225e05e9600e708
 use Illuminate\Http\Request;
 
 class PlanetController extends Controller
 {
-<<<<<<< HEAD
 	public function index($planet_id)
 	{
 		$planet = Planet::find($planet_id)->first();
-		$planet['films'];
+		$array = array();
 		foreach ($planet->films as $film) {
-			array_push($planet['films'], $film->film);
+			$array[] = $film->film;
 		}
+		$planet['film'] = $array;
 		return $planet;
 	}
-=======
-
-	public function index($id) {}
-
->>>>>>> a29bc2ed615844b5b4ac3c4cc225e05e9600e708
 }
