a7f2d6ddfd0357ba970ef1f380fb5689718d014a
Romain Neil <tutosfaciles48@gmail.com>
2021-10-06 13:31:59 +0200

    feat: add migration for pivot film specie

---
+++ database/migrations/2021_10_06_112103_create_pivot_film_species_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePivotFilmSpeciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pivot_film_species', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+			$table->foreign('film_id')->references('id')->on('films')
+				->onDelete('cascade')
+				->onUpdate('cascade');
+
+			$table->foreign('specie_id')->references('id')->on('species')
+				->onDelete('cascade')
+				->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pivot_film_species');
+    }
+}
