ac691863a47d4e8445a96e926395b6baadbdd11d
M1k3yD <85000131+M1k3yD@users.noreply.github.com>
2021-10-06 13:20:20 +0200

    Ajout des foreigns key du pivot films starships

--- database/migrations/2021_10_06_093507_create_pivot_film_starships_table.php
+++ database/migrations/2021_10_06_093507_create_pivot_film_starships_table.php
@@ -6,26 +6,37 @@ use Illuminate\Support\Facades\Schema;
 
 class CreatePivotFilmStarshipsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('pivot_film_starships', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('pivot_film_starships', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('pivot_film_starships');
-    }
+			$table->unsignedBigInteger('film_id');
+			$table->unsignedBigInteger('starship_id');
+
+			$table->foreign('film_id')->references('id')->on('films')
+				->onDelete('cascade')
+				->onUpdate('cascade');
+
+			$table->foreign('starship_id')->references('id')->on('starships')
+				->onDelete('cascade')
+				->onUpdate('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('pivot_film_starships');
+	}
 }
