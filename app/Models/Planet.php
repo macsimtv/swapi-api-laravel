82d89bc77b8b32c3426fe9ca4b1b808c74a4b2bc
Romain Neil <tutosfaciles48@gmail.com>
2021-10-06 10:45:56 +0200

    refactor(Planet): add fillable

---
+++ app/Models/Planet.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Planet extends Model {
+	use HasFactory;
+
+	protected $fillable = [
+		"name",
+		"rotation_period",
+		"orbital_period",
+		"diameter",
+		"climate",
+		"gravity",
+		"terrain",
+		"surface_water",
+		"population",
+		"residents",
+		"films",
+		"edited",
+		"url"
+	];
+
+	public function films(): BelongsToMany {
+		return $this->belongsToMany(Film::class);
+	}
+
+	public function planet_film(): HasMany {
+		return $this->hasMany(PivotPlanetFilm::class);
+	}
+
+}
