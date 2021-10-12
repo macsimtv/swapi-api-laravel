55fbe9dded6d9e501a90da00e33a8e1acecf1846
macsimtv <maximelecouturier@gmail.com>
2021-10-12 11:35:01 +0200

    PLANET API CONTROLLER [FINI]

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PivotPeoplePlanet;
 use App\Models\PivotPlanetFilm;
 use App\Models\Planet;
 use Illuminate\Http\Request;
@@ -11,13 +12,25 @@ class PlanetController extends Controller
 	public function index($planet_id)
 	{
 		$planet = Planet::find($planet_id);
+
+		// Peoples
+		$peoples = PivotPeoplePlanet::where('planet_id', $planet_id)->get();
+		$peopleArray = [];
+		foreach ($peoples as $people) {
+			$peopleArray[] = route('people', $people->people_id);
+		}
+		$planet['residents'] = $peopleArray;
+
+		// Films
 		$films = PivotPlanetFilm::where('planet_id', $planet->id)->get();
 		$filmsArray = [];
 		foreach ($films as $film) {
 			$filmsArray[] = route('planet', $film->film_id);
 		}
 		$planet['films'] = $filmsArray;
-		// $planet['films'] = $array;
+
+		$planet['url'] = route('planet', $planet_id);
+
 		return response()->json($planet);
 	}
 }
